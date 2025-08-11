e12d5299719de8077c86b626a2e47ad1c49ec7d2
GunGuyDude <kyleleungsauhay@gmail.com>
2025-08-12 00:32:43 +0800

    Added exclusive and inclusive animation containers

--- src/rpyl2dp/rpyl2dp.py
+++ src/rpyl2dp/rpyl2dp.py
@@ -1,6 +1,7 @@
 import os
 from os import path
 import json
+import queue
 
 global my_path
 my_path = "D:\\Tools\\Ren'Py\\projects\\Testing\\game\\live2d\\G2MimiruSprite"
@@ -9,6 +10,8 @@ class Model:
     def __init__(self, name: str):
         self.name = name
         self.motions = dict()
+        self.exclusive = Exclusive()
+        self.inclusive = Inclusive()
         return
     
     def __str__(self):
@@ -18,10 +21,29 @@ class Model:
             out += f'Motion name: {motion} '
         return out
     
+    def exclusive_push(self, wait_seconds: float, animation_name: str) -> None:
+        self.exclusive.push(wait_seconds, animation_name)
+        return
+    
+    def exclusive_pop(self) -> tuple[float, str]:
+        return self.exclusive.pop()
+    
+    def exclusive_empty(self) -> bool:
+        return self.exclusive.exclusive_queue.empty()
+    
+    def inclusive_add(self, min_seconds: float, max_seconds: float, animation_name: str) -> None:
+        self.inclusive.add(min_seconds, max_seconds, animation_name)
+        return
+    
+    def inclusive_remove(self, animation_name: str) -> None:
+        self.inclusive.remove(animation_name)
+        return
+
+    # WIP
     def play(self, motion_name: str) -> None:
         if not isinstance(motion_name, str):
             raise TypeError('Motion name must be a string')
-        elif not motion_name in self.motions:
+        elif motion_name not in self.motions:
             raise KeyError('No motion with the given name is associated with this model')
         else:
             if isinstance(self.motions[motion_name], Motion):
@@ -32,14 +54,14 @@ class Model:
                 raise TypeError('Motion has an unknown class')
         return
 
-# Find the value of every parameter of this motion at this second
+    # Find the value of every parameter of this motion at this second
     def second(self, motion_name: str, st: float) -> float:
         values = list()
         if not isinstance(motion_name, str):
             raise TypeError('Motion name must be a string')
         elif not (isinstance(st, float) or isinstance(st, int)):
             raise TypeError('Seconds must be a float')
-        elif not motion_name in self.motions:
+        elif motion_name not in self.motions:
             raise KeyError('No motion with the given name is associated with this model')
         else:
             if isinstance(self.motions[motion_name], Motion):
@@ -79,8 +101,21 @@ class Model:
             else:
                 raise TypeError('Motion has an unknown class')
         return values
-    
-class Motion:
+
+# Parent class for motions and animations
+class Animation:
+    def __init__(self, name: str):
+        if not isinstance(name, str):
+            raise TypeError('Name must be a string')
+        else:
+            self.name = name
+            return
+
+    def __str__(self):
+        return f'Name: {self.name}'
+
+# Class for motions
+class Motion(Animation):
     def __init__(self, name: str, duration: float, loop: bool, curves: list):
         if not isinstance(name, str):
             raise TypeError('Name must be a string')
@@ -100,7 +135,8 @@ class Motion:
     def __str__(self):
         return f'Name: {self.name} - Duration: {self.duration} - Loop: {self.loop} - Curves: {self.curves}'
 
-class Expression:
+# Class for expressions
+class Expression(Animation):
     def __init__(self, name: str, parameters: list):
         if not isinstance(name, str):
             raise TypeError('Name must be a string')
@@ -114,6 +150,54 @@ class Expression:
     def __str__(self):
         return f'Name: {self.name} - Parameters: {self.parameters}'
     
+# Exclusive animations use a FIFO queue. Exclusive animations can only play one at a time.
+class Exclusive:
+    def __init__(self):
+        self.exclusive_queue = queue.Queue()
+        return
+
+    def push(self, wait_seconds: float, animation_name: str) -> None:
+        if not isinstance(wait_seconds, float):
+            raise TypeError('Wait seconds must be a float')
+        elif not isinstance(animation_name, str):
+            raise TypeError('Animation name must be a string')
+        else:
+            self.exclusive_queue.put((wait_seconds, animation_name))
+            return
+    
+    def pop(self) -> tuple[float, str]:
+        if self.exclusive_queue.empty():
+            return None
+        else:
+            (wait_seconds, animation_name) = self.exclusive_queue.get()
+            return (wait_seconds, animation_name)
+
+# Inclusive animations use a dict. All inclusive animations in the dict can play simultaneously.
+class Inclusive:
+    def __init__(self):
+        self.inclusive_dict = dict()
+        return
+
+    def add(self, min_seconds: float, max_seconds: float, animation_name: str) -> None:
+        if not isinstance(min_seconds, float):
+            raise TypeError('Minimum seconds must be a float')
+        elif not isinstance(max_seconds, float):
+            raise TypeError('Maximum seconds must be a float')
+        elif not isinstance(animation_name, str):
+            raise TypeError('Animation name must be a string')
+        else:
+            self.inclusive_dict[animation_name] = (min_seconds, max_seconds)
+            return
+
+    def remove(self, animation_name: str) -> None:
+        if not isinstance(animation_name, str):
+            raise TypeError('Animation name must be a string')
+        elif animation_name not in self.inclusive_dict:
+            return
+        else:
+            self.inclusive_dict.pop(animation_name)
+            return
+
 # Static function
 # Load a Live2D model given its directory path
 def load_model(folder_path: str) -> Model:
@@ -163,7 +247,7 @@ def load_expression(file_path: str) -> Expression:
 
 # Static function
 # Solve for y given st (x) in a linear equation
-def linear(st: float, p0: tuple[float], p1: tuple[float]) -> float:
+def linear(st: float, p0: tuple[float, float], p1: tuple[float, float]) -> float:
     # Normalise st to t
     t = (st-p0[0]) / (p1[0]-p0[0])
     y = t*(p1[1]-p0[1]) + p0[1]
@@ -171,7 +255,7 @@ def linear(st: float, p0: tuple[float], p1: tuple[float]) -> float:
 
 # Static function
 # Solve for y given st (x) in a cubic bezier
-def cubic_bezier(st: float, p0: tuple[float], p1: tuple[float], p2: tuple[float], p3: tuple[float]) -> float:
+def cubic_bezier(st: float, p0: tuple[float, float], p1: tuple[float, float], p2: tuple[float, float], p3: tuple[float, float]) -> float:
     # Normalise st to t
     t = (st-p0[0]) / (p3[0]-p0[0])
     y = (1-t)**3 * p0[1] + 3*t*(1-t)**2 * p1[1] + 3*(1-t)*t**2 * p2[1] + t**3 * p3[1]
